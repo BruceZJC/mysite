1b1a2ff625a081c3e858c41baf4aceb8450055cb
Bruce_ZJ <59980547+BruceZJC@users.noreply.github.com>
2020-12-10 18:28:41 -0500

    removed irrelevant files

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from FirePredict import views
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    #path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('FirePredict/firedata_detail/<int:fire_id>',views.firedata_detail,name ="firedata_detail")
 ]
--- secondsite/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from . import models
-# Create your views here.
-def index(request):
-
-    return HttpResponse("<h3>标题")
